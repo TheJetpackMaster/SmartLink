d6441d6633931c5814bbbda3a7af88ac7d6d315c
Parvez Mayar <parvezbaloc32@gmail.com>
2025-05-05 19:36:35 +0500

    basic changes .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.smartlink.smartlinkapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.smartlink.smartlinkapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
